3b1d9cddd66543627a09e8199d410866cb2b7718
webtech <webtech@webtech-debian.webtech>
2023-06-10 15:48:20 -0400

    feat: agregar index de tweets

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :tweets, except: [:update, :edit]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
