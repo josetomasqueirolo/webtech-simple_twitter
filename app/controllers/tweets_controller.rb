6dbd139ed4ef90cd590dfd4c169dab8c70d438b3
webtech <webtech@webtech-debian.webtech>
2023-06-10 17:03:41 -0400

    feat: boton de destroy

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,15 +1,34 @@
 class TweetsController < ApplicationController
-    def index 
+    def index
         @tweets = Tweet.all
     end
 
     def show
-        @tweet = Tweet.find(params[:id])
+        @tweet = Tweet.find params[:id]
     end
 
-    def def new
+    def new
         @tweet = Tweet.new
     end
-    
-    
+
+    def create
+        tweet = Tweet.new tweet_params
+        tweet.user = current_user
+        if tweet.save
+            redirect_to tweet, notice: 'Tweet guardado con éxito'
+        else
+            render :new
+        end
+    end
+
+    def destroy
+        tweet = Tweet.find(params[:id])
+        tweet.destroy
+        redirect_to tweets_path, notice: "Eliminado con éxito"
+    end
+
+    private
+    def tweet_params
+        params.require(:tweet).permit(:body)
+    end
 end
